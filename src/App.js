c713ffd2e234bfecef3222aeadba315ec6ac572a
ross-u <cirkovic.uros.srb@gmail.com>
2021-12-07 00:38:00 +0000

    Initial commit - starter code

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import HomePage from "./pages/HomePage";
 import ProjectListPage from "./pages/ProjectListPage";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 import EditProjectPage from "./pages/EditProjectPage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
-      <Routes>      
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectListPage />} />
         <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
-        <Route path="/projects/edit/:projectId" element={<EditProjectPage />} />           
+        <Route path="/projects/edit/:projectId" element={<EditProjectPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
